e17f0c9b9488f591c1e18880938aa69379ccf3c2
thierry-dotcom <t.kochuyt@gmail.com>
2019-12-20 18:28:14 +0100

    Updates

---
+++ precache-manifest.a3ff4a9bda35981d4585bdaf67eaba0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6415d3964f950e9a93a488c7f89eccd7",
+    "url": "/money-converter/index.html"
+  },
+  {
+    "revision": "c39aac1b247186cb7d04",
+    "url": "/money-converter/static/css/main.ec3303f5.chunk.css"
+  },
+  {
+    "revision": "c8af9b9799ca323e8725",
+    "url": "/money-converter/static/js/2.08af8ea0.chunk.js"
+  },
+  {
+    "revision": "c39aac1b247186cb7d04",
+    "url": "/money-converter/static/js/main.00632445.chunk.js"
+  },
+  {
+    "revision": "ffd52bd548fb6aa9d5ed",
+    "url": "/money-converter/static/js/runtime-main.85bba5ea.js"
+  },
+  {
+    "revision": "5530d56d7b9abd643676e57a5282efda",
+    "url": "/money-converter/static/media/Money.5530d56d.jpg"
+  }
+]);
