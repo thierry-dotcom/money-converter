0d2b2c67e28fd4c1cb515d3417e76700d4dfcfd4
thierry-dotcom <t.kochuyt@gmail.com>
2019-12-20 18:19:49 +0100

    first commit

---
+++ build/precache-manifest.81860f484e48147ad4a85b2700cc3a33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cad682fd91ee507553c8a355c2365504",
+    "url": "/moneyConverter/index.html"
+  },
+  {
+    "revision": "08a9ecee5bc1071a7857",
+    "url": "/moneyConverter/static/css/main.12eba6fc.chunk.css"
+  },
+  {
+    "revision": "c8af9b9799ca323e8725",
+    "url": "/moneyConverter/static/js/2.08af8ea0.chunk.js"
+  },
+  {
+    "revision": "08a9ecee5bc1071a7857",
+    "url": "/moneyConverter/static/js/main.00632445.chunk.js"
+  },
+  {
+    "revision": "a113bcab441aaf163777",
+    "url": "/moneyConverter/static/js/runtime-main.e784f2f1.js"
+  },
+  {
+    "revision": "5530d56d7b9abd643676e57a5282efda",
+    "url": "/moneyConverter/static/media/Money.5530d56d.jpg"
+  }
+]);
